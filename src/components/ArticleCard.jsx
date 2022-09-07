06c849e56e054e7915b252c9462a09aafaa28993
Zack <schaxmann@gmail.com>
2022-09-07 23:14:50 +0100

    Updates ArticleCard styling

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -33,7 +33,7 @@ function ArticleCard(props) {
   }
   return (
     <li className="articleCard">
-      <h3>{article.title}</h3>
+      <h3>{article.title.toUpperCase()}</h3>
       <h4>
         Published: {formatDate(new Date(article.created_at.split("T")[0]))}
       </h4>
