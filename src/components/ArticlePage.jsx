edf10fe81559d8e5ebb3d2749a912e0efb52ecb4
Zack <schaxmann@gmail.com>
2022-09-09 15:52:56 +0100

    Adds styling to article elements

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import "../styling/ArticlePage.css";
 import { useFetchArticle } from "../hooks/useFetch";
 import Votes from "./Votes";
@@ -9,32 +9,75 @@ import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
 import CardOverflow from "@mui/joy/CardOverflow";
 import Typography from "@mui/joy/Typography";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faContactCard,
+  faHeart,
+  faSoccerBall,
+  faMessage,
+  faPenToSquare,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faBowlFood,
+  faCode,
+  faUtensils,
+  faUtensilSpoon,
+  faCaretRight,
+  faAnglesRight,
+  faRightLong,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ArticlePage() {
   const { article } = useParams();
   const { currentArticle, isLoading } = useFetchArticle(article);
   const teal = "teal";
 
+  function formatDate(date) {
+    var monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return monthNames[monthIndex] + " " + day + " " + year;
+  }
+
   if (isLoading) return <CircularProgress sx={{ color: teal }} />;
   return (
     <main>
       <article className="mainArticles">
-        <Card variant="outlined" sx={{ width: "100%" }}>
-          <CardOverflow></CardOverflow>
-          <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-            {/* <Link className="articleLink" to={`/articles/${article.article_id}`}> */}
-            {currentArticle.title}
-            {/* </Link> */}
-          </Typography>
-          <Typography level="body 1" sx={{ fontSize: "md", mt: 2 }}>
-            {/* <Link className="articleLink" to={`/articles/${article.article_id}`}> */}
-            {currentArticle.body}
-            {/* </Link> */}
-          </Typography>
-          <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-            Written by: {currentArticle.author}
-          </Typography>
-        </Card>
+        <h2>{currentArticle.title}</h2>
+        <h4>
+          <FontAwesomeIcon className="fa" icon={faCalendar} />{" "}
+          {formatDate(new Date(currentArticle.created_at.split("T")[0]))}
+        </h4>
+        <h4>
+          <FontAwesomeIcon className="fa" icon={faPenToSquare} />{" "}
+          {currentArticle.author}
+        </h4>
+        <p>{currentArticle.body}</p>
+        <h4>
+          Filed Under:{" "}
+          <Link className="topicLink" to={`/${currentArticle.topic}`}>
+            {currentArticle.topic[0].toUpperCase() +
+              currentArticle.topic.slice(1)}
+          </Link>
+        </h4>
       </article>
       <section className="votes">
         <Votes votes={currentArticle.votes} article={article} />
