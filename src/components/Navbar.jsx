5cdb794c97c5183f31b559bf90fdec1616bf57e7
Zack <schaxmann@gmail.com>
2022-09-18 19:31:03 +0100

    Completes responsive nav menu for small screeens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,6 +24,25 @@ function Navbar() {
       <Link to="/cooking" className="navLink">
         COOKING
       </Link>
+      <div className="dropdown">
+        <button class="dropbtn">
+          MENU <span className="arrowDown"></span>
+        </button>
+        <div class="dropdown-content">
+          <Link to="/" className="navLink">
+            ALL ARTICLES
+          </Link>
+          <Link to="/coding" className="navLink">
+            CODING
+          </Link>
+          <Link to="/football" className="navLink">
+            FOOTBALL
+          </Link>
+          <Link to="/cooking" className="navLink">
+            COOKING
+          </Link>
+        </div>
+      </div>
       <div className="user">
         <span className="loggedIn">LOGGED IN AS </span>
         <Link to="/user/grumpy19" className="home">
